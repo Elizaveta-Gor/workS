095497eb92043c4e46a03ab4dfeaff86c6fac3e9
Elizaveta Goroshinskaya <tandem200125@gmail.com>
2025-08-03 21:57:29 +0300

    Добавлен путь к Chrome

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,4 +1,84 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class CardDeliveryTest {
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @BeforeEach
+    void setUp() {
+        Configuration.holdBrowserOpen = false;
+        Configuration.headless = true;
+        Configuration.browserBinary = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSubmitFormWithValidData() {
+        String date = generateDate(3);
+
+        $("[data-test-id=city] input").setValue("Самара");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Анна-Софья Иванова");
+        $("[data-test-id=phone] input").setValue("+79001112233");
+        $("[data-test-id=agreement]").click();
+
+        $$("button").findBy(Condition.exactText("Забронировать")).click();
+
+        $("[data-test-id=notification]")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date));
+    }
+
+    @Test
+    void shouldSubmitFormWithDoubleName() {
+        String date = generateDate(5);
+
+        $("[data-test-id=city] input").setValue("Уфа");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Мария Иванова-Петрова");
+        $("[data-test-id=phone] input").setValue("+79005554433");
+        $("[data-test-id=agreement]").click();
+
+        $$("button").findBy(Condition.exactText("Забронировать")).click();
+
+        $("[data-test-id=notification]")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date));
+    }
+
+    @Test
+    void shouldSubmitFormWithNameContainingSpace() {
+        String date = generateDate(7);
+
+        $("[data-test-id=city] input").setValue("Красноярск");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Николай Иван");
+        $("[data-test-id=phone] input").setValue("+79007778899");
+        $("[data-test-id=agreement]").click();
+
+        $$("button").findBy(Condition.exactText("Забронировать")).click();
+
+        $("[data-test-id=notification]")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date));
+    }
 }
+
+
